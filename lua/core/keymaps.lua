64ed3e8043ceadbf0e8e7976e452b8d67de129a7
Eric R <eric.rodriguez@gusto.com>
2023-10-29 15:01:07 -0400

    added relative numberlines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -7,6 +7,8 @@ vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
 
+vim.opt.relativenumber = true
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
